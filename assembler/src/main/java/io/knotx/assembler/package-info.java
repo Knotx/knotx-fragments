ecb4220529844c1fe8b08e1929c9765d84020933
Maciej Laskowski <skejven@gmail.com>
2019-06-14 21:47:00 +0200

    move assembler to fragments repo

--- assembler/src/main/java/io/knotx/assembler/package-info.java
+++ assembler/src/main/java/io/knotx/assembler/package-info.java
@@ -13,14 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-dependencies {
-  "implementation"(platform("io.knotx:knotx-dependencies:${project.version}"))
-  "implementation"(group = "io.vertx", name = "vertx-core")
-  "implementation"(group = "io.vertx", name = "vertx-service-proxy")
-  "implementation"(group = "io.vertx", name = "vertx-rx-java2")
-  "implementation"(group = "io.vertx", name = "vertx-codegen")
+@ModuleGen(name = "knotx-assembler", groupPackage = "io.knotx")
+package io.knotx.assembler;
 
-  "testImplementation"("io.knotx:knotx-junit5:${project.version}")
-  "testImplementation"(group = "io.vertx", name = "vertx-junit5")
-  "testImplementation"(group = "io.vertx", name = "vertx-unit")
-}
+import io.vertx.codegen.annotations.ModuleGen;
