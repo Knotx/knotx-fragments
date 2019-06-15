7fc9d7d8e9375bb897318dfd16d21c08d4afa6c3
Maciej Laskowski <skejven@gmail.com>
2019-06-15 13:12:46 +0200

    switch builds to knotx-gradle-plugins

--- handler/engine/build.gradle.kts
+++ handler/engine/build.gradle.kts
@@ -16,142 +16,36 @@
 import org.nosphere.apache.rat.RatTask
 
 plugins {
-    id("java-library")
-    id("maven-publish")
-    id("signing")
-    id("jacoco")
+    id("io.knotx.java-library")
+    id("io.knotx.unit-test")
+    id("io.knotx.maven-publish")
+    id("io.knotx.jacoco")
     id("org.nosphere.apache.rat") version "0.4.0"
 }
 
-// -----------------------------------------------------------------------------
-// Dependencies
-// -----------------------------------------------------------------------------
-
-val junitTestCompile = configurations.create("junitTestCompile")
-
 dependencies {
+    implementation(platform("io.knotx:knotx-dependencies:${project.version}"))
+
     api(project(":knotx-fragments-handler-api"))
+
+    implementation(group = "io.vertx", name = "vertx-core")
+    implementation(group = "io.vertx", name = "vertx-service-proxy")
+    implementation(group = "io.vertx", name = "vertx-rx-java2")
 }
 
-junitTestCompile.extendsFrom(configurations.named("testImplementation").get())
-
-// -----------------------------------------------------------------------------
-// Source sets
-// -----------------------------------------------------------------------------
-sourceSets.named("main") {
-    java.srcDir("src/main/generated")
-}
-tasks.named<JavaCompile>("compileJava") {
-    options.annotationProcessorGeneratedSourcesDirectory = file("src/main/generated")
-}
-tasks.named<Delete>("clean") {
-    delete.add("src/main/generated")
-}
-sourceSets.create("junitTest") {
-    compileClasspath += sourceSets.named("main").get().output
-}
-sourceSets.named("test") {
-    compileClasspath += sourceSets.named("junitTest").get().output
-    runtimeClasspath += sourceSets.named("junitTest").get().output
-}
-
-
-// -----------------------------------------------------------------------------
-// Tasks
-// -----------------------------------------------------------------------------
 tasks {
     named<RatTask>("rat") {
-        excludes.addAll("**/*.json", "**/*.md", "**/*.adoc", "**/build/*", "**/out/*", "**/generated/*", "/src/test/resources/*")
+        excludes.addAll("*.yml", "*.md", "**/*.md", "**/build/*", "**/out/*", "**/generated/*", "**/*.adoc")
     }
     getByName("build").dependsOn("rat")
 }
 
-// -----------------------------------------------------------------------------
-// Publication
-// -----------------------------------------------------------------------------
-tasks.register<Jar>("sourcesJar") {
-    from(sourceSets.named("main").get().allJava)
-    classifier = "sources"
-}
-tasks.register<Jar>("javadocJar") {
-    from(tasks.named<Javadoc>("javadoc"))
-    classifier = "javadoc"
-}
-tasks.named<Javadoc>("javadoc") {
-    if (JavaVersion.current().isJava9Compatible) {
-        (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
-    }
-}
-tasks.register<Jar>("testJar") {
-    from(sourceSets.named("junitTest").get().output)
-    classifier = "tests"
-}
-
 publishing {
     publications {
-        create<MavenPublication>("mavenJava") {
-            artifactId = "knotx-fragments-engine"
+        withType(MavenPublication::class) {
             from(components["java"])
             artifact(tasks["sourcesJar"])
             artifact(tasks["javadocJar"])
-            artifact(tasks["testJar"])
-            pom {
-                name.set("Knot.x Fragments Engine")
-                description.set("Fragments Engine module containing map-reduce, graph engine implementation.")
-                url.set("http://knotx.io")
-                licenses {
-                    license {
-                        name.set("The Apache Software License, Version 2.0")
-                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                    }
-                }
-                developers {
-                    developer {
-                        id.set("marcinczeczko")
-                        name.set("Marcin Czeczko")
-                        email.set("https://github.com/marcinczeczko")
-                    }
-                    developer {
-                        id.set("skejven")
-                        name.set("Maciej Laskowski")
-                        email.set("https://github.com/Skejven")
-                    }
-                    developer {
-                        id.set("tomaszmichalak")
-                        name.set("Tomasz Michalak")
-                        email.set("https://github.com/tomaszmichalak")
-                    }
-                }
-                scm {
-                    connection.set("scm:git:git://github.com/Knotx/knotx-fragments-handler.git")
-                    developerConnection.set("scm:git:ssh://github.com:Knotx/knotx-fragments-handler.git")
-                    url.set("http://knotx.io")
-                }
-            }
-        }
-        repositories {
-            maven {
-                val releasesRepoUrl = "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
-                val snapshotsRepoUrl = "https://oss.sonatype.org/content/repositories/snapshots"
-                url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
-                credentials {
-                    username = if (project.hasProperty("ossrhUsername")) project.property("ossrhUsername")?.toString() else "UNKNOWN"
-                    password = if (project.hasProperty("ossrhPassword")) project.property("ossrhPassword")?.toString() else "UNKNOWN"
-                    println("Connecting with user: ${username}")
-                }
-            }
         }
     }
 }
-
-val subProjectPath = this.path
-signing {
-    setRequired {
-        gradle.taskGraph.hasTask("$subProjectPath:publish") ||
-                gradle.taskGraph.hasTask("$subProjectPath:publishMavenJavaPublicationToMavenRepository")
-    }
-
-    sign(publishing.publications["mavenJava"])
-}
-
-apply(from = "../../gradle/common.deps.gradle.kts")
--- supplier/html-splitter/build.gradle.kts
+++ supplier/html-splitter/build.gradle.kts
@@ -16,34 +16,27 @@
 import org.nosphere.apache.rat.RatTask
 
 plugins {
-    id("java-library")
-    id("maven-publish")
-    id("signing")
-    id("jacoco")
+    id("io.knotx.java-library")
+    id("io.knotx.codegen")
+    id("io.knotx.unit-test")
+    id("io.knotx.maven-publish")
+    id("io.knotx.jacoco")
     id("org.nosphere.apache.rat") version "0.4.0"
 }
 
-// -----------------------------------------------------------------------------
-// Dependencies
-// -----------------------------------------------------------------------------
 dependencies {
+    implementation(platform("io.knotx:knotx-dependencies:${project.version}"))
     api(project(":knotx-fragments-supplier-api"))
+
+    implementation(group = "io.vertx", name = "vertx-core")
+    implementation(group = "io.vertx", name = "vertx-service-proxy")
+    implementation(group = "io.vertx", name = "vertx-rx-java2")
     implementation(group = "org.apache.commons", name = "commons-lang3")
 
     testImplementation(group = "org.mockito", name = "mockito-core")
     testImplementation(group = "org.mockito", name = "mockito-junit-jupiter")
 }
 
-// -----------------------------------------------------------------------------
-// Source sets
-// -----------------------------------------------------------------------------
-sourceSets.named("main") {
-    java.srcDir("src/main/generated")
-}
-
-// -----------------------------------------------------------------------------
-// Tasks
-// -----------------------------------------------------------------------------
 tasks {
     named<RatTask>("rat") {
         excludes.addAll("*.md", "**/*.md", "**/build/*", "**/out/*", "**/generated/*", "/src/test/resources/*")
@@ -51,85 +44,12 @@ tasks {
     getByName("build").dependsOn("rat")
 }
 
-// -----------------------------------------------------------------------------
-// Publication
-// -----------------------------------------------------------------------------
-tasks.register<Jar>("sourcesJar") {
-    from(sourceSets.named("main").get().allJava)
-    classifier = "sources"
-}
-tasks.register<Jar>("javadocJar") {
-    from(tasks.named<Javadoc>("javadoc"))
-    classifier = "javadoc"
-}
-tasks.named<Javadoc>("javadoc") {
-    if (JavaVersion.current().isJava9Compatible) {
-        (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
-    }
-}
 publishing {
     publications {
-        create<MavenPublication>("mavenJava") {
-            artifactId = "knotx-fragments-supplier-html-splitter"
+        withType(MavenPublication::class) {
             from(components["java"])
             artifact(tasks["sourcesJar"])
             artifact(tasks["javadocJar"])
-            pom {
-                name.set("Knot.x Fragments Supplier HTML Splitter")
-                description.set("HTML Splitter - slices HTML template into fragments")
-                url.set("http://knotx.io")
-                licenses {
-                    license {
-                        name.set("The Apache Software License, Version 2.0")
-                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                    }
-                }
-                developers {
-                    developer {
-                        id.set("marcinczeczko")
-                        name.set("Marcin Czeczko")
-                        email.set("https://github.com/marcinczeczko")
-                    }
-                    developer {
-                        id.set("skejven")
-                        name.set("Maciej Laskowski")
-                        email.set("https://github.com/Skejven")
-                    }
-                    developer {
-                        id.set("tomaszmichalak")
-                        name.set("Tomasz Michalak")
-                        email.set("https://github.com/tomaszmichalak")
-                    }
-                }
-                scm {
-                    connection.set("scm:git:git://github.com/Knotx/knotx-fragments-handler.git")
-                    developerConnection.set("scm:git:ssh://github.com:Knotx/knotx-fragments-handler.git")
-                    url.set("http://knotx.io")
-                }
-            }
-        }
-        repositories {
-            maven {
-                val releasesRepoUrl = "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
-                val snapshotsRepoUrl = "https://oss.sonatype.org/content/repositories/snapshots"
-                url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
-                credentials {
-                    username = if (project.hasProperty("ossrhUsername")) project.property("ossrhUsername")?.toString() else "UNKNOWN"
-                    password = if (project.hasProperty("ossrhPassword")) project.property("ossrhPassword")?.toString() else "UNKNOWN"
-                    println("Connecting with user: ${username}")
-                }
-            }
         }
     }
 }
-val subProjectPath = this.path
-signing {
-    setRequired {
-        gradle.taskGraph.hasTask("$subProjectPath:publish") ||
-                gradle.taskGraph.hasTask("$subProjectPath:publishMavenJavaPublicationToMavenRepository")
-    }
-
-    sign(publishing.publications["mavenJava"])
-}
-
-apply(from = "../../gradle/common.deps.gradle.kts")
