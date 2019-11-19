a6d2aeda4a9a09b8c03a653f54edabf3e804dc80
Tomasz Michalak <tomek.michalak@gmail.com>
2019-11-19 09:01:04 +0100

    Changing configuration model

--- handler/core/src/test/java/io/knotx/fragments/handler/FragmentsHandlerTest.java
+++ handler/core/src/test/java/io/knotx/fragments/handler/FragmentsHandlerTest.java
@@ -16,11 +16,11 @@
 package io.knotx.fragments.handler;
 
 import static com.google.common.collect.Lists.newArrayList;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.when;
 
+import io.knotx.fragments.HoconLoader;
 import io.knotx.fragments.api.Fragment;
 import io.knotx.fragments.handler.options.FragmentsHandlerOptions;
 import io.knotx.junit5.util.FileReader;
@@ -33,7 +33,6 @@ import io.vertx.junit5.VertxTestContext;
 import io.vertx.reactivex.core.Vertx;
 import io.vertx.reactivex.ext.web.RoutingContext;
 import java.io.IOException;
-import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -51,57 +50,44 @@ class FragmentsHandlerTest {
   @DisplayName("Expect fail with Status Code 500 when any fragment is failed.")
   void shouldFail(Vertx vertx, VertxTestContext testContext)
       throws Throwable {
-    //given
-    RoutingContext routingContext = mockRoutingContext("failing-task");
+    HoconLoader.verify("tasks/fragment-handler/failingAction.conf", config -> {
+      //given
+      RoutingContext routingContext = mockRoutingContext("failing-task");
+      FragmentsHandler underTest = new FragmentsHandler(vertx, config);
 
-    FragmentsHandler underTest = new FragmentsHandler(vertx,
-        from("tasks/fragment-handler/failingAction.json"));
+      //when
+      underTest.handle(routingContext);
 
-    //when
-    underTest.handle(routingContext);
-
-    //then
-    doAnswer(invocation -> {
-      testContext.completeNow();
-      return null;
-    })
-        .when(routingContext)
-        .fail(500);
-
-    assertTrue(testContext.awaitCompletion(5, TimeUnit.SECONDS));
-
-    if (testContext.failed()) {
-      throw testContext.causeOfFailure();
-    }
-
+      //then
+      doAnswer(invocation -> {
+        testContext.completeNow();
+        return null;
+      })
+          .when(routingContext)
+          .fail(500);
+    }, testContext, vertx);
   }
 
   @Test
   @DisplayName("Expect continuing processing next handler when no fragment is failed.")
   void shouldSuccess(Vertx vertx, VertxTestContext testContext)
       throws Throwable {
-    //given
-    RoutingContext routingContext = mockRoutingContext("success-task");
+    HoconLoader.verify("tasks/fragment-handler/successAction.conf", config -> {
+      //given
+      RoutingContext routingContext = mockRoutingContext("success-task");
+      FragmentsHandler underTest = new FragmentsHandler(vertx, config);
 
-    FragmentsHandler underTest = new FragmentsHandler(vertx,
-        from("tasks/fragment-handler/successAction.json"));
+      //when
+      underTest.handle(routingContext);
 
-    //when
-    underTest.handle(routingContext);
-
-    //then
-    doAnswer(invocation -> {
-      testContext.completeNow();
-      return null;
-    })
-        .when(routingContext)
-        .next();
-
-    assertTrue(testContext.awaitCompletion(5, TimeUnit.SECONDS));
-
-    if (testContext.failed()) {
-      throw testContext.causeOfFailure();
-    }
+      //then
+      doAnswer(invocation -> {
+        testContext.completeNow();
+        return null;
+      })
+          .when(routingContext)
+          .next();
+    }, testContext, vertx);
   }
 
   private RoutingContext mockRoutingContext(String task) {
