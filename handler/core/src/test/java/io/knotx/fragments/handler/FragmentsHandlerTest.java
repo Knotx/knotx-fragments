8ae5876c061c190d18011cc7215b70287a1e5a9d
Tomasz Michalak <tomek.michalak@gmail.com>
2019-11-21 12:46:46 +0100

    Fragments handler tests cleanup.

--- handler/core/src/test/java/io/knotx/fragments/handler/FragmentsHandlerTest.java
+++ handler/core/src/test/java/io/knotx/fragments/handler/FragmentsHandlerTest.java
@@ -17,7 +17,6 @@ package io.knotx.fragments.handler;
 
 import static com.google.common.collect.Lists.newArrayList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.when;
@@ -29,7 +28,6 @@ import io.knotx.fragments.engine.FragmentEvent.Status;
 import io.knotx.fragments.handler.exception.TaskFactoryNameNotDefinedException;
 import io.knotx.fragments.handler.exception.TaskFactoryNotFoundException;
 import io.knotx.fragments.task.factory.DefaultTaskFactory;
-import io.knotx.junit5.util.FileReader;
 import io.knotx.server.api.context.ClientRequest;
 import io.knotx.server.api.context.RequestContext;
 import io.knotx.server.api.context.RequestEvent;
@@ -39,7 +37,6 @@ import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
 import io.vertx.reactivex.core.Vertx;
 import io.vertx.reactivex.ext.web.RoutingContext;
-import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
@@ -55,36 +52,14 @@ import org.mockito.quality.Strictness;
 @MockitoSettings(strictness = Strictness.LENIENT)
 class FragmentsHandlerTest {
 
-  public static final String CUSTOM_TASK_NAME_KEY = "task";
-  public static final String EMPTY_BODY = "";
-
-  @Test
-  @DisplayName("Expect fail with Status Code 500 when any fragment is failed.")
-  void shouldFail(Vertx vertx, VertxTestContext testContext)
-      throws Throwable {
-    HoconLoader.verify("handler/singleDefaultTaskFactoryWithFailingTask.conf", config -> {
-      //given
-      RoutingContext routingContext = mockRoutingContext("failing-task");
-      FragmentsHandler underTest = new FragmentsHandler(vertx, config);
-
-      //when
-      underTest.handle(routingContext);
-
-      //then
-      doAnswer(invocation -> {
-        testContext.completeNow();
-        return null;
-      })
-          .when(routingContext)
-          .fail(500);
-    }, testContext, vertx);
-  }
+  private static final String CUSTOM_TASK_NAME_KEY = "task";
+  private static final String EMPTY_BODY = "";
 
   @Test
   @DisplayName("Expect continuing processing next handler when no fragment is failed.")
   void shouldSuccess(Vertx vertx, VertxTestContext testContext)
       throws Throwable {
-    HoconLoader.verify("handler/singleDefaultTaskFactoryWithSuccessTask.conf", config -> {
+    HoconLoader.verify("handler/singleTaskFactoryWithSuccessTask.conf", config -> {
       //given
       RoutingContext routingContext = mockRoutingContext("success-task");
       FragmentsHandler underTest = new FragmentsHandler(vertx, config);
@@ -103,10 +78,84 @@ class FragmentsHandlerTest {
   }
 
   @Test
-  @DisplayName("Expect updated fragment when two default factories defined.")
-  void twoDefaultFactories(Vertx vertx, VertxTestContext testContext)
+  @DisplayName("Expect fail with Status Code 500 when any fragment is failed.")
+  void shouldFail(Vertx vertx, VertxTestContext testContext)
       throws Throwable {
-    HoconLoader.verify("handler/manyDefaultTaskFactoriesWithSuccessTask.conf", config -> {
+    HoconLoader.verify("handler/singleTaskFactoryWithFailingTask.conf", config -> {
+      //given
+      RoutingContext routingContext = mockRoutingContext("failing-task");
+      FragmentsHandler underTest = new FragmentsHandler(vertx, config);
+
+      //when
+      underTest.handle(routingContext);
+
+      //then
+      doAnswer(invocation -> {
+        testContext.completeNow();
+        return null;
+      })
+          .when(routingContext)
+          .fail(500);
+    }, testContext, vertx);
+  }
+
+  @Test
+  @DisplayName("Expect processed fragment when factory accepts fragment.")
+  void singleFactory(Vertx vertx, VertxTestContext testContext)
+      throws Throwable {
+    HoconLoader.verify("handler/singleTaskFactoryWithSuccessTask.conf", config -> {
+      //given
+      FragmentsHandler underTest = new FragmentsHandler(vertx, config);
+      Fragment fragment = new Fragment("type",
+          new JsonObject().put(DefaultTaskFactory.DEFAULT_TASK_NAME_KEY, "success-task"), EMPTY_BODY);
+      String expectedBody = "success";
+
+      //when
+      Single<List<FragmentEvent>> rxDoHandle = underTest
+          .doHandle(Collections.singletonList(fragment), new ClientRequest());
+
+      rxDoHandle.subscribe(
+          result -> testContext.verify(() -> {
+            // then
+            FragmentEvent fragmentEvent = result.get(0);
+            assertEquals(Status.SUCCESS, fragmentEvent.getStatus());
+            assertEquals(expectedBody, fragmentEvent.getFragment().getBody());
+            testContext.completeNow();
+          }),
+          testContext::failNow
+      );
+    }, testContext, vertx);
+  }
+
+  @Test
+  @DisplayName("Expect unprocessed fragment when factory does not accept fragment.")
+  void singleFactoryNotAcceptingFragment(Vertx vertx, VertxTestContext testContext)
+      throws Throwable {
+    HoconLoader.verify("handler/singleTaskFactoryWithSuccessTask.conf", config -> {
+      //given
+      FragmentsHandler underTest = new FragmentsHandler(vertx, config);
+      Fragment fragment = new Fragment("type", new JsonObject(), EMPTY_BODY);
+
+      //when
+      Single<List<FragmentEvent>> rxDoHandle = underTest
+          .doHandle(Collections.singletonList(fragment), new ClientRequest());
+
+      rxDoHandle.subscribe(
+          result -> testContext.verify(() -> {
+            // then
+            assertEquals(Status.UNPROCESSED, result.get(0).getStatus());
+            testContext.completeNow();
+          }),
+          testContext::failNow
+      );
+    }, testContext, vertx);
+  }
+
+  @Test
+  @DisplayName("Expect processed fragment when second factory accepts fragment.")
+  void twoFactoriesWithTheSameName(Vertx vertx, VertxTestContext testContext)
+      throws Throwable {
+    HoconLoader.verify("handler/manyTaskFactoriesWithTheSameName.conf", config -> {
       //given
       FragmentsHandler underTest = new FragmentsHandler(vertx, config);
       Fragment fragment = new Fragment("type",
@@ -119,11 +168,7 @@ class FragmentsHandlerTest {
 
       rxDoHandle.subscribe(
           result -> testContext.verify(() -> {
-            assertFalse(result.isEmpty());
-            assertEquals(1, result.size());
-            FragmentEvent fragmentEvent = result.get(0);
-            assertEquals(Status.SUCCESS, fragmentEvent.getStatus());
-            assertEquals(expectedBody, fragmentEvent.getFragment().getBody());
+            assertEquals(expectedBody, result.get(0).getFragment().getBody());
             testContext.completeNow();
           }),
           testContext::failNow
@@ -132,7 +177,7 @@ class FragmentsHandlerTest {
   }
 
   @Test
-  @DisplayName("Expect exception when task factory name not defined")
+  @DisplayName("Expect exception when task factory name is not defined")
   void taskFactoryNameNotDefined(Vertx vertx, VertxTestContext testContext)
       throws Throwable {
     HoconLoader.verify("handler/taskFactoryNameNotDefined.conf", config -> {
@@ -146,7 +191,7 @@ class FragmentsHandlerTest {
   }
 
   @Test
-  @DisplayName("Expect exception when task factory name not found")
+  @DisplayName("Expect exception when task factory name is not found")
   void taskFactoryNotFound(Vertx vertx, VertxTestContext testContext)
       throws Throwable {
     HoconLoader.verify("handler/taskFactoryNotFound.conf", config -> {
@@ -174,9 +219,5 @@ class FragmentsHandlerTest {
   private Fragment fragment(String task) {
     return new Fragment("type",
         new JsonObject().put(DefaultTaskFactory.DEFAULT_TASK_NAME_KEY, task), "");
-  }
-
-  private JsonObject from(String fileName) throws IOException {
-    return new JsonObject(FileReader.readText(fileName));
   }
 }
