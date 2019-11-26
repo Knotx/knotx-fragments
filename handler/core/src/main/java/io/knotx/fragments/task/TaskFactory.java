acc8ceb37141b4491cb886545098c63817410a41
Tomasz Michalak <tomek.michalak@gmail.com>
2019-11-26 15:56:40 +0100

    Add TaskFactory javadocs.

--- handler/core/src/main/java/io/knotx/fragments/task/TaskFactory.java
+++ handler/core/src/main/java/io/knotx/fragments/task/TaskFactory.java
@@ -17,16 +17,48 @@ package io.knotx.fragments.task;
 
 import io.knotx.fragments.engine.FragmentEventContext;
 import io.knotx.fragments.engine.Task;
+import io.knotx.fragments.handler.FragmentsHandlerOptions;
 import io.vertx.core.json.JsonObject;
 import io.vertx.reactivex.core.Vertx;
 
+/**
+ * A task factory interface allowing to register a task factory by its name. Implementing class must
+ * be configured in <code>META-INF.services</code>.
+ *
+ * Task factories are configured in {@link FragmentsHandlerOptions#getTaskFactories()}.
+ */
 public interface TaskFactory {
 
+  /**
+   * @return task factory name
+   */
   String getName();
 
-  TaskFactory configure(JsonObject factoryConfig, Vertx vertx);
+  /**
+   * Configures a task factory with config defined in {@link TaskFactoryOptions#getConfig()}. This
+   * method is called during factories initialization.
+   *
+   * @param config json configuration, see {@link TaskFactoryOptions#getConfig()}
+   * @param vertx vertx instance
+   * @return a reference to this, so the API can be used fluently
+   */
+  TaskFactory configure(JsonObject config, Vertx vertx);
 
-  boolean accept(FragmentEventContext eventContext);
+  /**
+   * Determines if a fragment event context can be processed by the factory.
+   *
+   * @param context fragment event context
+   * @return <code>true</code> when accepted
+   */
+  boolean accept(FragmentEventContext context);
 
-  Task newInstance(FragmentEventContext eventContext);
+  /**
+   * Creates the new task instance. It is called only if {@link #accept(FragmentEventContext)}
+   * returns <code>true</code>. When called with a fragment that does not provide a task name, then
+   * {@link io.knotx.fragments.task.exception.TaskNotFoundException} is thrown.
+   *
+   * @param context fragment event context
+   * @return new task instance
+   */
+  Task newInstance(FragmentEventContext context);
 }
