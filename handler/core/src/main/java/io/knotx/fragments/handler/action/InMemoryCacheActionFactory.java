13c6662250907426dc14f8ac29d6ff046351c044
marcin.szymura <marcin.szymura@cognifide.com>
2020-01-13 13:10:42 +0100

    #76 Provides logging for InMemoryCacheActionFactory

--- handler/core/src/main/java/io/knotx/fragments/handler/action/InMemoryCacheActionFactory.java
+++ handler/core/src/main/java/io/knotx/fragments/handler/action/InMemoryCacheActionFactory.java
@@ -16,19 +16,17 @@
 package io.knotx.fragments.handler.action;
 
 
-import java.util.concurrent.TimeUnit;
-
-import org.apache.commons.lang3.StringUtils;
-
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
-
 import io.knotx.fragments.api.Fragment;
 import io.knotx.fragments.handler.api.Action;
 import io.knotx.fragments.handler.api.ActionFactory;
 import io.knotx.fragments.handler.api.Cacheable;
+import io.knotx.fragments.handler.api.actionlog.ActionLogLevel;
+import io.knotx.fragments.handler.api.actionlog.ActionLogger;
 import io.knotx.fragments.handler.api.domain.FragmentContext;
 import io.knotx.fragments.handler.api.domain.FragmentResult;
+import io.knotx.fragments.handler.helper.TimeCalculator;
 import io.knotx.server.api.context.ClientRequest;
 import io.knotx.server.common.placeholders.PlaceholdersResolver;
 import io.knotx.server.common.placeholders.SourceDefinitions;
@@ -37,6 +35,9 @@ import io.vertx.core.Future;
 import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
+import java.time.Instant;
+import java.util.concurrent.TimeUnit;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * Payload Cache Action factory class. It can be initialized with a configuration:
@@ -56,6 +57,12 @@ import io.vertx.core.json.JsonObject;
  */
 @Cacheable
 public class InMemoryCacheActionFactory implements ActionFactory {
+
+  public static final String CACHE_LOOKUP = "CACHE_LOOKUP";
+  public static final String CACHE_MISS = "CACHE_MISS";
+  public static final String CACHE_HIT = "CACHE_HIT";
+  public static final String CACHE_PASS = "CACHE_PASS";
+  public static final String TRANSITION = "TRANSITION";
 
   private static final long DEFAULT_MAXIMUM_SIZE = 1000;
   private static final long DEFAULT_TTL = 5000;
@@ -78,33 +85,47 @@ public class InMemoryCacheActionFactory implements ActionFactory {
           Handler<AsyncResult<FragmentResult>> resultHandler) {
 
         String cacheKey = getCacheKey(config, fragmentContext.getClientRequest());
+
+        ActionLogger actionLogger = ActionLogger.create(alias, ActionLogLevel.INFO);
+        logCacheLookup(actionLogger, cacheKey);
+
         Object cachedValue = cache.getIfPresent(cacheKey);
         if (cachedValue == null) {
-          callDoActionAndCache(fragmentContext, resultHandler, cacheKey);
+          callDoActionAndCache(fragmentContext, resultHandler, cacheKey, actionLogger);
         } else {
+          logCacheHit(actionLogger, cachedValue);
           Fragment fragment = fragmentContext.getFragment();
           fragment.appendPayload(payloadKey, cachedValue);
-          FragmentResult result = new FragmentResult(fragment, FragmentResult.SUCCESS_TRANSITION);
+          FragmentResult result = new FragmentResult(fragment, FragmentResult.SUCCESS_TRANSITION,
+              actionLogger.toLog().toJson());
           Future.succeededFuture(result)
               .setHandler(resultHandler);
         }
       }
 
       private void callDoActionAndCache(FragmentContext fragmentContext,
-          Handler<AsyncResult<FragmentResult>> resultHandler, String cacheKey) {
+          Handler<AsyncResult<FragmentResult>> resultHandler, String cacheKey,
+          ActionLogger actionLogger) {
+        long startTime = Instant.now().toEpochMilli();
         doAction.apply(fragmentContext, asyncResult -> {
           if (asyncResult.succeeded()) {
             FragmentResult fragmentResult = asyncResult.result();
+            actionLogger
+                .doActionLog(TimeCalculator.executionTime(startTime), fragmentResult.getNodeLog());
             if (FragmentResult.SUCCESS_TRANSITION.equals(fragmentResult.getTransition())
                 && fragmentResult.getFragment()
                 .getPayload()
                 .containsKey(payloadKey)) {
-              JsonObject resultPayload = fragmentResult.getFragment()
-                  .getPayload();
-              cache.put(cacheKey, resultPayload.getMap()
-                  .get(payloadKey));
+              Object resultPayload = fragmentResult.getFragment()
+                  .getPayload().getMap().get(payloadKey);
+              logCacheMiss(actionLogger, resultPayload);
+              cache.put(cacheKey, resultPayload);
+            } else {
+              logCachePass(actionLogger, fragmentResult);
             }
-            Future.succeededFuture(fragmentResult)
+            Future.succeededFuture(
+                new FragmentResult(fragmentResult.getFragment(), fragmentResult.getTransition(),
+                    actionLogger.toLog().toJson()))
                 .setHandler(resultHandler);
           } else {
             Future.<FragmentResult>failedFuture(asyncResult.cause()).setHandler(resultHandler);
@@ -146,5 +167,22 @@ public class InMemoryCacheActionFactory implements ActionFactory {
         .maximumSize(maxSize)
         .expireAfterWrite(ttl, TimeUnit.MILLISECONDS)
         .build();
+  }
+
+  private static void logCacheLookup(ActionLogger actionLogger, String cacheKey) {
+    actionLogger.info(CACHE_LOOKUP, cacheKey);
+  }
+
+  private static void logCacheHit(ActionLogger actionLogger, Object cachedValue) {
+    actionLogger.info(CACHE_HIT, cachedValue);
+  }
+
+  private static void logCacheMiss(ActionLogger actionLogger, Object computedValue) {
+    actionLogger.info(CACHE_MISS, computedValue);
+  }
+
+  private static void logCachePass(ActionLogger actionLogger, FragmentResult failedResult) {
+    actionLogger.info(CACHE_PASS,
+        new JsonObject().put(TRANSITION, failedResult.getTransition()));
   }
 }
---
+++ handler/core/src/test/java/io/knotx/fragments/handler/action/logging/InMemoryCacheActionFactoryLoggingTest.java
@@ -0,0 +1,208 @@
+package io.knotx.fragments.handler.action.logging;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import io.knotx.fragments.api.Fragment;
+import io.knotx.fragments.handler.action.InMemoryCacheActionFactory;
+import io.knotx.fragments.handler.api.Action;
+import io.knotx.fragments.handler.api.domain.FragmentContext;
+import io.knotx.fragments.handler.api.domain.FragmentResult;
+import io.knotx.junit5.KnotxExtension;
+import io.knotx.server.api.context.ClientRequest;
+import io.vertx.core.Future;
+import io.vertx.core.json.JsonObject;
+import io.vertx.junit5.VertxTestContext;
+import java.util.concurrent.TimeUnit;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(KnotxExtension.class)
+class InMemoryCacheActionFactoryLoggingTest {
+
+  private static final String EXAMPLE_CACHE_KEY = "cProduct";
+
+  private static final String ACTION_ALIAS = "action";
+  private static final String PAYLOAD_KEY = "product";
+  private static final String LOGS_KEY = "logs";
+
+  private static final JsonObject ACTION_CONFIG = new JsonObject().put("payloadKey", PAYLOAD_KEY)
+      .put("cacheKey", EXAMPLE_CACHE_KEY);
+
+  private Fragment firstFragment;
+  private Fragment secondFragment;
+
+  @BeforeEach
+  void setUp() {
+    firstFragment = new Fragment("type", new JsonObject(), "initial body");
+    secondFragment = new Fragment("type", new JsonObject(), "initial body");
+  }
+
+  @DisplayName("Cache lookup gets logged with cache key")
+  @Test
+  void callActionWithSuccessTransitionLog(VertxTestContext testContext) throws Throwable {
+    // given
+    Action doAction = (fragmentContext, resultHandler) -> Future
+        .succeededFuture(new FragmentResult(firstFragment, FragmentResult.SUCCESS_TRANSITION))
+        .setHandler(resultHandler);
+
+    Action tested = new InMemoryCacheActionFactory()
+        .create(ACTION_ALIAS, ACTION_CONFIG, null, doAction);
+
+    // when
+    tested.apply(new FragmentContext(firstFragment, new ClientRequest()),
+        result -> {
+          // then
+          testContext.verify(() -> {
+            JsonObject log = result.result().getNodeLog().getJsonObject(LOGS_KEY);
+            assertTrue(log.containsKey(InMemoryCacheActionFactory.CACHE_LOOKUP));
+            assertEquals(EXAMPLE_CACHE_KEY, log.getString(InMemoryCacheActionFactory.CACHE_LOOKUP));
+            testContext.completeNow();
+          });
+        });
+
+    assertTrue(testContext.awaitCompletion(5, TimeUnit.SECONDS));
+    if (testContext.failed()) {
+      throw testContext.causeOfFailure();
+    }
+  }
+
+
+  @DisplayName("Cache miss gets logged with computed value")
+  @Test
+  void callActionWithPayloadUpdate(VertxTestContext testContext) throws Throwable {
+    // given
+    JsonObject expectedPayloadValue = new JsonObject().put("someKey", "someValue");
+    Action doAction = (fragmentContext, resultHandler) -> {
+      Fragment fragment = fragmentContext.getFragment();
+      fragment.appendPayload(PAYLOAD_KEY, expectedPayloadValue);
+      Future
+          .succeededFuture(new FragmentResult(fragment, FragmentResult.SUCCESS_TRANSITION))
+          .setHandler(resultHandler);
+    };
+
+    Action tested = new InMemoryCacheActionFactory()
+        .create(ACTION_ALIAS, ACTION_CONFIG, null, doAction);
+
+    // when
+    tested.apply(new FragmentContext(firstFragment, new ClientRequest()),
+        result -> {
+          // then
+          testContext.verify(() -> {
+            JsonObject log = result.result().getNodeLog().getJsonObject(LOGS_KEY);
+            assertTrue(log.containsKey(InMemoryCacheActionFactory.CACHE_MISS));
+            assertEquals(expectedPayloadValue,
+                log.getJsonObject(InMemoryCacheActionFactory.CACHE_MISS));
+            testContext.completeNow();
+          });
+        });
+
+    assertTrue(testContext.awaitCompletion(5, TimeUnit.SECONDS));
+    if (testContext.failed()) {
+      throw testContext.causeOfFailure();
+    }
+  }
+
+  @DisplayName("Cache hit gets logged with cached value")
+  @Test
+  void callActionTwiceWithTheSameKey(VertxTestContext testContext) throws Throwable {
+    // given
+    JsonObject expectedPayloadValue = new JsonObject().put("someKey", "someValue");
+    Action doAction = (fragmentContext, resultHandler) -> {
+      Fragment fragment = fragmentContext.getFragment();
+      fragment.appendPayload(PAYLOAD_KEY, expectedPayloadValue);
+      Future
+          .succeededFuture(new FragmentResult(fragment, FragmentResult.SUCCESS_TRANSITION))
+          .setHandler(resultHandler);
+    };
+
+    Action tested = new InMemoryCacheActionFactory()
+        .create(ACTION_ALIAS, ACTION_CONFIG, null, doAction);
+
+    FragmentContext firstRequestContext = new FragmentContext(firstFragment, new ClientRequest());
+    FragmentContext secondRequestContext = new FragmentContext(secondFragment, new ClientRequest());
+
+    // when
+    tested.apply(firstRequestContext,
+        firstResult -> tested.apply(
+            secondRequestContext, secondResult -> {
+              // then
+              testContext.verify(() -> {
+                JsonObject log = secondResult.result().getNodeLog().getJsonObject(LOGS_KEY);
+                assertTrue(log.containsKey(InMemoryCacheActionFactory.CACHE_HIT));
+                assertEquals(expectedPayloadValue,
+                    log.getJsonObject(InMemoryCacheActionFactory.CACHE_HIT));
+                testContext.completeNow();
+              });
+            })
+    );
+
+    assertTrue(testContext.awaitCompletion(5, TimeUnit.SECONDS));
+    if (testContext.failed()) {
+      throw testContext.causeOfFailure();
+    }
+  }
+
+  @DisplayName("Cache pass gets logged on _error transition from doAction")
+  @Test
+  void callActionWithErrorTransition(VertxTestContext testContext) throws Throwable {
+    Action doAction = (fragmentContext, resultHandler) -> Future
+        .succeededFuture(
+            new FragmentResult(fragmentContext.getFragment(), FragmentResult.ERROR_TRANSITION))
+        .setHandler(resultHandler);
+
+    Action tested = new InMemoryCacheActionFactory()
+        .create(ACTION_ALIAS, ACTION_CONFIG, null, doAction);
+
+    // when
+    tested.apply(new FragmentContext(firstFragment, new ClientRequest()),
+        result -> {
+          // then
+          testContext.verify(() -> {
+            JsonObject log = result.result().getNodeLog().getJsonObject(LOGS_KEY);
+            assertTrue(log.containsKey(InMemoryCacheActionFactory.CACHE_PASS));
+            assertEquals(FragmentResult.ERROR_TRANSITION,
+                log.getJsonObject(InMemoryCacheActionFactory.CACHE_PASS)
+                    .getString(InMemoryCacheActionFactory.TRANSITION));
+            testContext.completeNow();
+          });
+        });
+
+    assertTrue(testContext.awaitCompletion(5, TimeUnit.SECONDS));
+    if (testContext.failed()) {
+      throw testContext.causeOfFailure();
+    }
+  }
+
+  @DisplayName("Cache pass gets logged when no payload is added by doAction")
+  @Test
+  void callActionThatDoesNotAddPayload(VertxTestContext testContext) throws Throwable {
+    Action doAction = (fragmentContext, resultHandler) -> Future
+        .succeededFuture(
+            new FragmentResult(fragmentContext.getFragment(), FragmentResult.SUCCESS_TRANSITION))
+        .setHandler(resultHandler);
+
+    Action tested = new InMemoryCacheActionFactory()
+        .create(ACTION_ALIAS, ACTION_CONFIG, null, doAction);
+
+    // when
+    tested.apply(new FragmentContext(firstFragment, new ClientRequest()),
+        result -> {
+          // then
+          testContext.verify(() -> {
+            JsonObject log = result.result().getNodeLog().getJsonObject(LOGS_KEY);
+            assertTrue(log.containsKey(InMemoryCacheActionFactory.CACHE_PASS));
+            testContext.completeNow();
+          });
+        });
+
+    assertTrue(testContext.awaitCompletion(5, TimeUnit.SECONDS));
+    if (testContext.failed()) {
+      throw testContext.causeOfFailure();
+    }
+  }
+
+}
