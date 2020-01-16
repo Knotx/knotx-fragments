a9bc76f38a522ca9359405186dc44f2fafcd10d7
Oskar Jerzyk <56078027+oskarjerzyk@users.noreply.github.com>
2020-01-16 13:03:42 +0100

    #85 HTTP Action log implementation.

--- handler/api/src/main/java/io/knotx/fragments/handler/api/actionlog/ActionLogBuilder.java
+++ handler/api/src/main/java/io/knotx/fragments/handler/api/actionlog/ActionLogBuilder.java
@@ -18,45 +18,43 @@ package io.knotx.fragments.handler.api.actionlog;
 import static io.knotx.fragments.handler.api.actionlog.ActionInvocationLog.error;
 import static io.knotx.fragments.handler.api.actionlog.ActionInvocationLog.success;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Stream;
-
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
+import java.util.ArrayList;
 
 class ActionLogBuilder {
+
   private String alias;
   private JsonObject logs;
   private ArrayList<ActionInvocationLog> doActionLogs;
 
-  ActionLogBuilder(String alias){
+  ActionLogBuilder(String alias) {
     this.alias = alias;
     this.logs = new JsonObject();
     this.doActionLogs = new ArrayList<>();
   }
 
-  ActionLogBuilder appendInvocationLogEntry(long duration, ActionLog actionLog){
+  void appendInvocationLogEntry(long duration, ActionLog actionLog) {
     doActionLogs.add(success(duration, actionLog));
-    return this;
   }
 
-  ActionLogBuilder appendFailureInvocationLogEntry(long duration, ActionLog actionLog){
-    doActionLogs.add(error(duration,  actionLog));
-    return this;
+  void appendFailureInvocationLogEntry(long duration, ActionLog actionLog) {
+    doActionLogs.add(error(duration, actionLog));
   }
 
-  ActionLogBuilder addLog(String key, String value){
+  void addLog(String key, String value) {
     logs.put(key, value);
-    return this;
   }
 
-  ActionLogBuilder addLog(String key, JsonObject value){
+  void addLog(String key, JsonObject value) {
     logs.put(key, value);
-    return this;
   }
 
-  ActionLog build(){
-    Stream.of(doActionLogs.toArray());
+  void addLog(String key, JsonArray value) {
+    logs.put(key, value);
+  }
+
+  ActionLog build() {
     return new ActionLog(alias, logs, doActionLogs);
   }
 }
