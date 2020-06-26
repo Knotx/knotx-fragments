d6f998b07b5d7defc91d45e204baff151615f0fa
Maciej Laskowski <Skejven@users.noreply.github.com>
2020-06-26 09:41:11 +0200

    Knotx/knotx-stack#57 run tests for included builds

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,7 +21,7 @@ pluginManagement {
         id("io.knotx.unit-test") version version
         id("io.knotx.jacoco") version version
         id("io.knotx.maven-publish") version version
-        id("io.knotx.publish-all-composite") version version
+        id("io.knotx.composite-build-support") version version
         id("io.knotx.release-java") version version
         id("org.nosphere.apache.rat") version "0.6.0"
     }
