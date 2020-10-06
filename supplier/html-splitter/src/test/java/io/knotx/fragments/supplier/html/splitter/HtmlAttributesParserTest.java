113cb23d82f29ad7c593c45f03144e7b0007e578
Marcin Szymura <marcin.szymura@cognifide.com>
2020-10-06 12:23:31 +0200

    #200 Remove unresolved imports

--- supplier/html-splitter/src/test/java/io/knotx/fragments/supplier/html/splitter/HtmlAttributesParserTest.java
+++ supplier/html-splitter/src/test/java/io/knotx/fragments/supplier/html/splitter/HtmlAttributesParserTest.java
@@ -25,8 +25,6 @@ import org.apache.commons.lang3.tuple.Pair;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 class HtmlAttributesParserTest {
 
